048a492547a6bc047dbf88bdaff6db24921c6b7a
waldan-r <waldan.rafid@ui.ac.id>
2025-09-24 16:43:04 +0700

    update setting.py

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "waldan-rafid-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://waldan-rafid-footbalnews.pbp.cs.ui.ac.id"
+    "https://waldan-rafid-footballnews.pbp.cs.ui.ac.id"
 ]
 # Application definition
 
